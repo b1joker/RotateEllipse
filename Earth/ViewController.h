320d04bd0e7da0240f73e31543d8527d86ae6f61
Lam Nguyen <nhlam.225@gmail.com>
2014-07-27 14:55:44 +0700

    Initial Commit

---
+++ Earth/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Earth
+//
+//  Created by Gin on 7/27/14.
+//  Copyright (c) 2014 Nguyễn Huỳnh Lâm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
