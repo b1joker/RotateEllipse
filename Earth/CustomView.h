53a76bb31174cecc31ea5d2e81f3fc0d831da718
Lam Nguyen <nhlam.225@gmail.com>
2014-07-29 11:16:14 +0700

    first commit

---
+++ Earth/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  Earth
+//
+//  Created by Gin on 7/27/14.
+//  Copyright (c) 2014 Nguyễn Huỳnh Lâm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
